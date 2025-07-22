e4f87d3b441bf93d1fe3c8f3bcfc0324cf0ab3ed
Marcel Maier <marcel.maier@lit-beratung.com>
2025-07-22 13:02:34 +0000

    remove auto preview in neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -23,16 +23,6 @@ return {
 			},
 			event_handlers = {
 				{
-					event = 'after_render',
-					handler = function()
-						local state = require('neo-tree.sources.manager').get_state('filesystem')
-						if not require('neo-tree.sources.common.preview').is_active() then
-							state.config = { use_float = false } -- or whatever your config is
-							state.commands.toggle_preview(state)
-						end
-					end
-				},
-				{
 					event = "file_open_requested",
 					handler = function()
 						require("neo-tree.command").execute({ action = "close" })
