4a0d37d27dadf261b2cdb2c1122a3cbece8fc695
Marcel Maier <marcel.maier@lit-beratung.com>
2025-07-25 16:10:50 +0000

    add undodir persistence

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,6 @@ vim.keymap.set('n', '<leader>cn', '<cmd>cnext<CR>', { desc = 'Next quickfix item
 vim.keymap.set('n', '<leader>cp', '<cmd>cprev<CR>', { desc = 'Previous quickfix item' })
 
 vim.opt.cmdheight = 0
+
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("config") .. "/undodir"
