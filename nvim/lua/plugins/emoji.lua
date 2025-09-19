b633fb8df3b480a06553c65764ab46d582f630a1
Marcel Maier <marcel.maier@lit-beratung.com>
2025-09-19 08:31:36 +0000

    allow emoji in all filetypes

--- nvim/lua/plugins/emoji.lua
+++ nvim/lua/plugins/emoji.lua
@@ -1,7 +1,6 @@
 return {
 	"allaman/emoji.nvim",
 	version = "1.0.0", -- optionally pin to a tag
-	ft = "markdown", -- adjust to your needs
 	dependencies = {
 		-- util for handling paths
 		"nvim-lua/plenary.nvim",
