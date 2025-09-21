a21f6f527adc6f6cb3e2d970e2289f4edd3fc080
Marcel Maier <marcel.maier@lit-beratung.com>
2025-09-21 10:50:28 +0000

    load local vaults from gitignored file

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,25 +1,20 @@
 return {
 	'epwalsh/obsidian.nvim',
-	version = '*', -- recommended, use latest release instead of latest commit
+	version = '*',
 	lazy = true,
 	ft = 'markdown',
-	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	-- event = {
-	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-	--   -- refer to `:h file-pattern` for more examples
-	--   "BufReadPre path/to/my-vault/*.md",
-	--   "BufNewFile path/to/my-vault/*.md",
-	-- },
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 	},
-	opts = {
-		workspaces = {
-			{ name = 'private', path = '~/vaults/cellexec-vault/' },
-		},
-		ui = { enable = false },
-	},
+	opts = function(_, _)
+		-- Try to load ./local/obsidian-vaults.lua (relative to this plugin file)
+		local ok, local_workspaces = pcall(require, 'plugins.local.obsidian-vaults')
+
+		return {
+			workspaces = ok and local_workspaces,
+			ui = { enable = false },
+		}
+	end,
 	keys = {
 		{ '<leader>ob',  ':ObsidianBacklinks<CR>',   desc = '[B]acklinks' },
 		{ '<leader>oc',  ':ObsidianTOC<CR>',         desc = '[C]urrent TOC' },
