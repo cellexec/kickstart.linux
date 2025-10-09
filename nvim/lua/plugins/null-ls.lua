2451e4a2a8cb4e4a6ef7fd76aeeaa200a30dfe4c
Marcel Maier <marcel.maier@lit-beratung.com>
2025-10-09 11:38:16 +0000

    remove null-ls

--- nvim/lua/plugins/null-ls.lua
+++
@@ -1,9 +0,0 @@
--- LSP diagnostics, code actions, etc.
--- https://github.com/nvimtools/none-ls.nvim
-return {
-	"nvimtools/none-ls.nvim",
-	event = "VeryLazy",
-	opts = function()
-		return require "null-ls"
-	end,
-}
