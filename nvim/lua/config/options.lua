ebc8b4b6551fdb181f058ef923bede2a630c50e5
Marcel Maier <marcel.maier@lit-beratung.com>
2025-12-05 11:45:46 +0000

    add relative nubmers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,6 +6,7 @@ vim.opt.termguicolors = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.wrap = false
 vim.opt.updatetime = 500
